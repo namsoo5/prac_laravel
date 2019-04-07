b5f4d5945d13f5f10aa7e0c1845c7ae2aa4e097f
김남수 <are34@naver.com>
2019-04-08 00:00:12 +0900

    라우팅

--- myapp/routes/api.php
+++ myapp/routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 #라우트이름 home생성
-Route::get('/h', [
+Route::get('/my/name', [
     'as' => 'home',
     function () {
         return '내이름은 김남수!';
@@ -30,6 +30,6 @@ Route::get('/{name}', function ($name) {
 });
 
 #생성해놓은 라우터의이름을이용해서 리다이렉트
-Route::get('/home', function () {
+Route::get('/home/main', function () {
     return redirect(route('home'));
 });
