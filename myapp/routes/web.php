0e49f6b89ffdd6fb66400b013f733d6cb33bfe02
namsoo5 <are34@naver.com>
2019-04-08 19:30:15 +0900

    controller

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -12,6 +12,8 @@
 */
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+#Route::get('/', function () {
+#    return view('welcome');
+#});
+
+Route::get('/', 'WelcomeController@index');
